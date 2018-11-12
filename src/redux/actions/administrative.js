79ad0669331a54823c4c23a85d3123a4b183bce1
jameskirkpatrick <jkirkpatrick1990@gmail.com>
2018-11-11 21:55:16 -0500

    removes unused code

--- src/redux/actions/administrative.js
+++ src/redux/actions/administrative.js
@@ -2,31 +2,11 @@ import * as constants from '../constants';
 import database, { app } from '../../firebase';
 import _ from 'lodash';
 
-const setValuesList = values => dispatch => {
-  dispatch({ type: constants.Value.SET_VALUES_LIST, values });
-};
 const setAlignmentsList = alignments => dispatch => {
   dispatch({ type: constants.Alignment.SET_ALIGNMENTS_LIST, alignments });
 };
-const setQuirksList = quirks => dispatch => {
-  dispatch({ type: constants.Quirk.SET_QUIRKS_LIST, quirks });
-};
-const setOccupationsList = occupations => dispatch => {
-  dispatch({ type: constants.Occupation.SET_OCCUPATIONS_LIST, occupations });
-};
-const setRacesList = races => dispatch => {
-  dispatch({ type: constants.Race.SET_RACES_LIST, races });
-};
 const setGendersList = genders => dispatch => {
   dispatch({ type: constants.Gender.SET_GENDERS_LIST, genders });
-};
-export const updateCollectionList = (item, type, constant) => (
-  dispatch,
-  getState
-) => {
-  const updatedState = { ...getState()[type].all };
-  updatedState[item.id] = item;
-  dispatch({ type: constant, [type]: updatedState });
 };
 
 export const loadCollection = (type, uid) => (dispatch, getState) => {
@@ -52,20 +32,3 @@ export const loadCollection = (type, uid) => (dispatch, getState) => {
       .catch(err => reject(err));
   });
 };
-
-// export const createCollection = (collection, type) => (dispatch, getState) => {
-//   collection.forEach(item => {
-//     database
-//       .collection(type)
-//       .add({ name: item })
-//       .then(res =>
-//         dispatch(
-//           updateCollectionList(
-//             { id: res.id, name: item },
-//             type,
-//             constants[_.capitalize(type)][`SET_${_.toUpper(type)}_LIST`]
-//           )
-//         )
-//       );
-//   });
-// };
