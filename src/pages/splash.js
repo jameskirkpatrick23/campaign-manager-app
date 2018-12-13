aae5ad3556fadce9afd9fe20502928f95e48eb7f
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2018-12-13 13:23:56 -0500

    updates logo and favicon to match tabletop chronicler rebranding

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import Logo from '../assets/Roll4InitiativeLogo.svg';
+import Logo from '../assets/Logo-Inverse.svg';
 import { Jumbotron, Button } from 'react-bootstrap';
 import Campaigns from './campaigns';
 
